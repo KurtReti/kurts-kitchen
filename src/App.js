999b2e67cacf4623200a1d06c817ed88524f65b2
KurtReti <68139188+KurtReti@users.noreply.github.com>
2023-08-19 16:52:00 +1000

    added active letter styling

--- src/App.js
+++ src/App.js
@@ -6,39 +6,50 @@ import axios from "axios";
 
 function App() {
   const [recipe, setRecipe] = useState([]);
-  const [currentPageURL, setCurrentPageURL] = useState("https://www.themealdb.com/api/json/v1/1/search.php?f=a")
-  const [activeLetter, setActiveLetter] = useState("a")
-  const [activeRecipe, setActiveRecipe] = useState("")
+  const [currentPageURL, setCurrentPageURL] = useState(
+    "https://www.themealdb.com/api/json/v1/1/search.php?f=a"
+  );
+  const [activeLetter, setActiveLetter] = useState("a");
+  const [activeRecipe, setActiveRecipe] = useState("");
+  
 
-  // return recipes from API
+  // return recipes from API using axios
   useEffect(() => {
-  axios
-    .get(currentPageURL)
-    .then((res) => {
-      setRecipe(
-        res.data.meals.map((r) => (
-          <>
-            <img className="self-center" height="200" width="200" src={r.strMealThumb} alt={r.strMeal} />
-            <div>{r.strMeal}</div>
-          </>
-        ))
-      );
-    });
-  }, [currentPageURL])
+    axios
+      .get(currentPageURL)
+      .then((res) => {
+        setRecipe(
+          res.data.meals.map((r) => (
+            <div onClick={setActiveRecipe}>
+              <img
+                className="self-center"
+                height="200"
+                width="200"
+                src={r.strMealThumb}
+                alt={r.strMeal}
+              />
+              <div>{r.strMeal}</div>
+            </div>
+          ))
+        );
+      }).catch((error) => {
+        alert("No recipes exist for this letter. Please try another letter.")
+      })
+  }, [currentPageURL]);
 
+  
   function setLetter(letter) {
-    setCurrentPageURL(`https://www.themealdb.com/api/json/v1/1/search.php?f=${letter}`)
-    setActiveLetter(letter)
-    window.scrollTo(0, 0)
+    setCurrentPageURL(
+      `https://www.themealdb.com/api/json/v1/1/search.php?f=${letter}`
+    );
+    setActiveLetter(letter);
+    window.scrollTo(0, 0);
   }
-
 
   return (
     <div className="">
       <RecipeList recipe={recipe} />
       <Footer setLetter={setLetter} activeLetter={activeLetter} />
-
-
     </div>
   );
 }
